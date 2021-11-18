89544c4941a3ce8faba7fc28a9ab74fc0b9d3a67
Igor Kioresko <ikioresko@gmail.com>
2021-11-18 12:55:54 +0500

    6. ResponseEntity [#504799]

--- chat/src/main/java/ru/job4j/chat/controller/RoomController.java
+++ chat/src/main/java/ru/job4j/chat/controller/RoomController.java
@@ -1,14 +1,17 @@
 package ru.job4j.chat.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.MultiValueMapAdapter;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import ru.job4j.chat.model.Room;
 import ru.job4j.chat.service.RoomService;
 
-import java.util.List;
+import java.util.*;
 
-@RestController
+@Controller
 @RequestMapping("/room")
 public class RoomController {
     private final RoomService rooms;
@@ -26,14 +29,26 @@ public class RoomController {
         return this.rooms.save(room);
     }
 
-    @GetMapping("/")
-    public List<Room> findAll() {
-        return rooms.findAll();
+    @GetMapping("/{id}")
+    public ResponseEntity<?> findRoomById(@PathVariable int id) {
+        return ResponseEntity.of(Optional.of(new HashSet<>() {{
+            add(rooms.findById(id).orElseThrow(() -> new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "Room is not found.")));
+        }}));
     }
 
-    @GetMapping("/{id}")
-    public Room findRoomById(@PathVariable int id) {
-        return rooms.findById(id).orElseThrow(() -> new ResponseStatusException(
-                HttpStatus.NOT_FOUND, "Room is not found."));
+    @GetMapping("/")
+    public ResponseEntity<?> findAll() {
+        Object body = new HashSet<>() {{
+            for (Room r : rooms.findAll()) {
+                add(r);
+            }
+        }};
+        var entity = new ResponseEntity(
+                body,
+                new MultiValueMapAdapter<>(Map.of("Job4jCustomHeader", List.of("job4j"))),
+                HttpStatus.OK
+        );
+        return entity;
     }
 }
